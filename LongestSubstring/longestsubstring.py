d5ddd8c50758a50e478f9f62f1e2b244ea6e544d
Ethan Phan <ethanphan3993@gmail.com>
2020-07-12 14:14:36 +1000

    First set up for the problem

---
+++ LongestSubstring/longestsubstring.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_length = 1
+        return max_length
+
+
+s = Solution()
+p = s.lengthOfLongestSubstring("hihi")
+print(p)
