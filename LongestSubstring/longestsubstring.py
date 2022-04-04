cfbb844e1a9a8d9c9cbcd78e1f4813b1b8e4d87f
Ethan Phan <ethan@tech-connect.com.au>
2022-04-04 17:05:19 +1000

    Rearrange files

--- LongestSubstring/longestsubstring.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        max_length = 1
-        return max_length
-
-
-s = Solution()
-p = s.lengthOfLongestSubstring("hihi")
-print(p)
